b6133c8d996f82f20b7c15d4f6913789a674f25f
simodewit <117769294+simodewit@users.noreply.github.com>
2023-03-27 21:19:57 +0200

    buttons en gridcreation toegevoegd

---
+++ Game Met Sem/Assets/Scripts/GridMaker.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class GridMaker : MonoBehaviour
+{
+    public float width;
+    public float length;
+    public GameObject Node1;
+    public GameObject Node2;
+    private Vector3 placePosition;
+    public quaternion rotation;
+
+    private void Start()
+    {
+        for(int i = 0; i < width; i++)
+        {
+
+            for( int j = 0; j < length; j++)
+            {
+                placePosition.x = i + transform.position.x;
+                placePosition.z = j + transform.position.z;
+                placePosition.y = transform.position.y;
+
+                Instantiate(Node2, placePosition, rotation, transform.parent);
+            }
+        }
+    }
+}
