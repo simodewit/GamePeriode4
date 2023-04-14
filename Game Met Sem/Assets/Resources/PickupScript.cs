5f8d9c5279493ef1be65dab621f0194b2349da9a
simodewit <117769294+simodewit@users.noreply.github.com>
2023-04-14 23:43:20 +0200

    blueprints kan je nu oppakken/droppen

--- Game Met Sem/Assets/Resources/PickupScript.cs
+++ Game Met Sem/Assets/Resources/PickupScript.cs
@@ -6,30 +6,51 @@ public class PickupScript : MonoBehaviour
 {
     private RaycastHit hit;
     private RaycastHit hit1;
+    private RaycastHit hit2;
     public Vector3 distanceToObject;
     private bool pickedUpSomething;
     public GameObject empty;
     private bool check;
     private Vector3 placePosition;
+    public float radius;
+    private bool blueprint;
+    public Vector3 blueprintPlacePosition;
+    public Quaternion blueprintRotation;
 
     public void Update()
     {
         check = true;
+
         if (Input.GetMouseButtonDown(0))
         {
             if (pickedUpSomething == true)
             {
-                if (Physics.Raycast(hit.transform.position, -hit.transform.up, out hit1, 5))
+                if(blueprint == true)
                 {
                     hit.transform.parent = null;
                     placePosition = hit1.transform.position;
-                    placePosition.y += 1;
+                    placePosition += blueprintPlacePosition;
                     hit.transform.position = placePosition;
                     hit.transform.localScale = new Vector3(1, 1, 1);
                     hit.collider.enabled = true;
                     pickedUpSomething = false;
                     check = false;
-                    hit.transform.localRotation = new Quaternion(0, 0, 0, 0);
+                    hit.transform.localRotation = blueprintRotation;
+                }
+                else
+                {
+                    if (Physics.Raycast(hit.transform.position, -hit.transform.up, out hit1, 5))
+                    {
+                        hit.transform.parent = null;
+                        placePosition = hit1.transform.position;
+                        placePosition.y += 1;
+                        hit.transform.position = placePosition;
+                        hit.transform.localScale = new Vector3(1, 1, 1);
+                        hit.collider.enabled = true;
+                        pickedUpSomething = false;
+                        check = false;
+                        hit.transform.localRotation = new Quaternion(0, 0, 0, 0);
+                    }
                 }
             }
 
@@ -48,6 +69,23 @@ public class PickupScript : MonoBehaviour
                             hit.transform.localRotation = new Quaternion(0, 0, 0, 0);
                             hit.transform.localScale = new Vector3(1, 1, 1);
                         }
+                    }
+                }
+            }
+
+            if(Physics.SphereCast(transform.position, radius, transform.forward, out hit2, 5))
+            {
+                if(hit2.transform.tag == "Blueprint")
+                {
+                    if(pickedUpSomething == false)
+                    {
+                        hit.collider.enabled = false;
+                        pickedUpSomething = true;
+                        hit.transform.SetParent(empty.transform);
+                        hit.transform.localPosition = new Vector3(0, 0, 0);
+                        hit.transform.localRotation = new Quaternion(0, 0, 0, 0);
+                        hit.transform.localScale = new Vector3(1, 1, 1);
+                        blueprint = true;
                     }
                 }
             }
