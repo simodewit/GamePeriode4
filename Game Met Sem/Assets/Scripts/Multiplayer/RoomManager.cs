730e115432b2f1bde1127310e3ff2ede7b6ca241
SemmNuesink <semm.nuesink@gmail.com>
2023-04-15 16:13:03 +0200

    push

--- Game Met Sem/Assets/Scripts/Multiplayer/RoomManager.cs
+++ Game Met Sem/Assets/Scripts/Multiplayer/RoomManager.cs
@@ -36,6 +36,6 @@ public class RoomManager : MonoBehaviourPunCallbacks
 
     public override void OnJoinedRoom()
     {
-        PhotonNetwork.LoadLevel("LobbyRoom");
+        PhotonNetwork.LoadLevel("TestPickUp");
     }
 }
