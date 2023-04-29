1a3cd950a201f0accd411a8ae2cfda70301d32f9
simodewit <117769294+simodewit@users.noreply.github.com>
2023-04-29 23:13:57 +0200

    Rpc

--- Game Met Sem/Assets/Scripts/PickupScript.cs
+++ Game Met Sem/Assets/Scripts/PickupScript.cs
@@ -1,25 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using Photon.Pun;
+using Unity.Burst.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PickupScript : MonoBehaviourPun
 {
-    [PunRPC]
-    private RaycastHit hit;
-    private RaycastHit hit1;
-    private RaycastHit hit2;
-    private RaycastHit hit3;
-    public Vector3 distanceToObject;
-    private bool pickedUpSomething;
-    public GameObject empty;
-    private bool check;
-    private Vector3 placePosition;
-    public float radius;
-    private bool blueprint;
-    public Vector3 blueprintPlacePosition;
-    public Quaternion blueprintRotation;
     public PhotonView view;
+    public GameObject empty;
+    private bool inHands;
+    private bool isPickup;
+    private bool isBlueprint;
+    private RaycastHit hitNode;
+    private RaycastHit hitPickup;
 
     public void Start()
     {
@@ -28,101 +23,62 @@ public class PickupScript : MonoBehaviourPun
 
     public void Update()
     {
-        if (view.IsMine)
-        { 
-            check = true;
-            if (Input.GetButtonDown("Fire1"))
-            {
-                print("1");
-                if (pickedUpSomething == true)
-                {
-                    print("2");
-                    if (Physics.Raycast(empty.transform.position, -empty.transform.up, out hit1, 5))
-                    {
-                        print("3");
-                        if (hit1.transform.GetComponent<Node>().occupied == false)
-                        {
-                            print("4");
-                            if (blueprint == true)
-                            {
-                                hit.transform.parent = null;
-                                placePosition = hit1.transform.position;
-                                placePosition += blueprintPlacePosition;
-                                hit.transform.position = placePosition;
-                                hit.transform.localScale = new Vector3(1, 1, 1);
-                                hit.collider.enabled = true;
-                                pickedUpSomething = false;
-                                check = false;
-                                hit.transform.localRotation = blueprintRotation;
-                                hit1.transform.GetComponent<Node>().occupied = true;
-                                print("5");
-                            }
-                            else
-                            {
-                                hit.transform.parent = null;
-                                placePosition = hit1.transform.position;
-                                placePosition.y += 1;
-                                hit.transform.position = placePosition;
-                                hit.transform.localScale = new Vector3(1, 1, 1);
-                                hit.collider.enabled = true;
-                                pickedUpSomething = false;
-                                check = false;
-                                hit.transform.localRotation = new Quaternion(0, 0, 0, 0);
-                                hit1.transform.GetComponent<Node>().occupied = true;
-                                print("5");
-                            }
-                        }
-                    }
-                }
+        if(!view.IsMine)
+            return;
 
-                if (check == true)
-                {
-                    print("11");
-                    if (pickedUpSomething == false)
-                    {
-                        print("12");
-                        if (Physics.Raycast(transform.position, transform.forward, out hit, 1.5f))
-                        {
-                            print("13");
-                            if (hit.transform.tag == "Pickup")
-                            {
-                                if (Physics.Raycast(empty.transform.position, -empty.transform.up, out hit3, 5))
-                                {
-                                    print("14");
-                                    hit.collider.enabled = false;
-                                    pickedUpSomething = true;
-                                    hit.transform.SetParent(empty.transform);
-                                    hit.transform.localPosition = new Vector3(0, 0, 0);
-                                    hit.transform.localRotation = new Quaternion(0, 0, 0, 0);
-                                    hit.transform.localScale = new Vector3(1, 1, 1);
-                                    hit3.transform.GetComponent<Node>().occupied = false;
-                                }
-                            }
-                        }
-                    }
-                }
+        if(!Input.GetButtonDown("Fire1"))
+            return;
 
-                if (Physics.SphereCast(transform.position, radius, transform.forward, out hit2, 5))
-                {
-                    print("21");
-                    if (hit2.transform.tag == "Blueprint")
-                    {
-                        print("22");
-                        if (pickedUpSomething == false)
-                        {
-                            print("23");
-                            hit.collider.enabled = false;
-                            pickedUpSomething = true;
-                            hit.transform.SetParent(empty.transform);
-                            hit.transform.localPosition = new Vector3(0, 0, 0);
-                            hit.transform.localRotation = new Quaternion(0, 0, 0, 0);
-                            hit.transform.localScale = new Vector3(1, 1, 1);
-                            blueprint = true;
-                        }
-                    }
-                }
-            }
+        if (!inHands)
+        {
+            view.RPC("PickupObject", RpcTarget.All);
         }
+        else
+        {
+            view.RPC("DropObject", RpcTarget.All);
+        }
+    }
+
+
+    [PunRPC]
+    public void PickupObject()
+    {
+        Physics.Raycast(transform.position, transform.forward, out hitPickup, 2);
+
+        if (hitPickup.transform.tag != "Pickup")
+            return;
+
+        Physics.Raycast(empty.transform.position, -empty.transform.up, out hitNode);
+
+        hitPickup.collider.enabled = false;
+        inHands = true;
+        hitPickup.transform.SetParent(empty.transform);
+        hitPickup.transform.localPosition = Vector3.zero;
+        hitPickup.transform.localRotation = Quaternion.identity;
+        hitPickup.transform.localScale = Vector3.one;
+        hitNode.transform.GetComponent<Node>().occupied = false;
+        isPickup = true;
+    }
+
+    [PunRPC]
+    public void DropObject()
+    {
+        if(!isPickup)
+            return;
+
+        Physics.Raycast(empty.transform.position, -empty.transform.up, out hitNode, 2);
+
+        if (hitNode.transform.GetComponent<Node>().occupied)
+            return;
+
+        hitPickup.transform.parent = null;
+        hitPickup.collider.enabled = true;
+        hitPickup.transform.localPosition = hitNode.transform.position + new Vector3 (0,1,0);
+        hitPickup.transform.localRotation = Quaternion.identity;
+        hitPickup.transform.localScale = Vector3.one;
+        hitNode.transform.GetComponent<Node>().occupied = true;
+        inHands = false;
+        isPickup = false;
     }
 }
     
