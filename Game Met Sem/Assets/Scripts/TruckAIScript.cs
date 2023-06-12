1e48e4acb3a0bccb83517a03a81ee5abf79844f1
simodewit <s.f.buikema@gmail.com>
2023-06-12 15:32:15 +0100

    trucks fix

---
+++ Game Met Sem/Assets/Scripts/TruckAIScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TruckAIScript : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public GameObject place1;
+    public GameObject place2;
+    public GameObject place3;
+    public GameObject place4;
+
+    public void Update()
+    {
+        
+    }
+}
