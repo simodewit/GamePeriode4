de337efddc98a5aae95c6349f3adc55301c703a7
simodewit <s.f.buikema@gmail.com>
2023-06-13 11:27:03 +0100

    Truck script af op bug na

--- Game Met Sem/Assets/Scripts/DeleteTruck.cs
+++ Game Met Sem/Assets/Scripts/DeleteTruck.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class DeleteTruck : MonoBehaviour
 {
+    public WaveSpawnerScript script;
+    public void Start()
+    {
+        script = GameObject.Find("WaveSpawner").GetComponent<WaveSpawnerScript>();
+    }
+
     public void OnCollisionEnter(Collision collision)
     {
+        script.trucksInGame.Remove(collision.gameObject);
         Destroy(collision.gameObject);
     }
 }
