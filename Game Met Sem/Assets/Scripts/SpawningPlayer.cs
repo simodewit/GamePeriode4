75577b4e87f15d08405733516ecf4818c8327485
simodewit <s.f.buikema@gmail.com>
2023-06-13 13:52:43 +0100

    possible fix

--- Game Met Sem/Assets/Scripts/SpawningPlayer.cs
+++ Game Met Sem/Assets/Scripts/SpawningPlayer.cs
@@ -8,10 +8,18 @@ public class SpawningPlayer : MonoBehaviour
 {
     public GameObject player;
     public TMP_Text playerCount;
+    public PhotonView view;
+
     void Awake()
     {
-        player = PhotonNetwork.Instantiate("Player 1", transform.position, Quaternion.identity);  
+        view.RPC("test", RpcTarget.All);
     }
+
+    public void test()
+    {
+        player = PhotonNetwork.Instantiate("Player 1", transform.position, Quaternion.identity);
+    }
+
     private void Update()
     {
         playerCount.text = PhotonNetwork.CurrentRoom.PlayerCount.ToString();
