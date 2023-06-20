602b5a4da72448a153cecb00d11df4b84a89360d
simodewit <117769294+simodewit@users.noreply.github.com>
2023-06-20 20:40:08 +0200

    WERK

--- Game Met Sem/Assets/Scripts/SpawningPlayer.cs
+++ Game Met Sem/Assets/Scripts/SpawningPlayer.cs
@@ -8,72 +8,143 @@ using Photon.Realtime;
 
 public class SpawningPlayer : MonoBehaviourPunCallbacks
 {
+    //public PhotonView view;
+    //public GameObject playerPrefab;
+    //public GameObject currentPlayer;
+    //public GameObject hostPlayer;
+    //public GameObject[] players;
+    //public Material[] materials;
+    //public GameObject panelHostLeave;
+    //public GameObject panelMoreThanFourPlayers;
+
+    //public void Start()
+    //{
+    //    view = gameObject.GetComponent<PhotonView>();
+    //    panelHostLeave = GameObject.Find("HostLeave");
+    //    panelMoreThanFourPlayers = GameObject.Find("FivePlayers");
+
+    //    if(!view.IsMine)
+    //        return;
+
+    //    if (PhotonNetwork.CurrentRoom.PlayerCount != 1)
+    //        view.RPC("OtherPlayer", RpcTarget.All);
+
+    //    view.RPC("FirstPlayer", RpcTarget.All);
+    //}
+
+    //public void Update()
+    //{
+    //    if(hostPlayer == null)
+    //    {
+    //        panelHostLeave.SetActive(true);
+    //    }
+    //}
+
+    //public void FirstPlayer()
+    //{
+    //    currentPlayer = PhotonNetwork.Instantiate("PlayerCharacter Variant", transform.position, Quaternion.identity);
+    //    currentPlayer.GetComponent<Renderer>().material = materials[1];
+    //    players[PhotonNetwork.CurrentRoom.PlayerCount] = playerPrefab;
+    //    gameObject.name = "MainPlayer";
+    //}
+
+    //public void OtherPlayer()
+    //{
+    //    currentPlayer = PhotonNetwork.Instantiate("PlayerCharacter Variant", new Vector3 (0,1,0), Quaternion.identity);
+    //    hostPlayer = GameObject.Find("MainPlayer");
+
+    //    if (hostPlayer.GetComponent<SpawningPlayer>().players[2] == null)
+    //    {
+    //        gameObject.name = "Player2";
+    //        hostPlayer.GetComponent<SpawningPlayer>().players[2] = playerPrefab;
+    //        currentPlayer.GetComponent<Renderer>().material = materials[2];
+    //    }
+    //    else if (hostPlayer.GetComponent<SpawningPlayer>().players[3] == null)
+    //    {
+    //        gameObject.name = "Player3";
+    //        hostPlayer.GetComponent<SpawningPlayer>().players[3] = playerPrefab;
+    //        currentPlayer.GetComponent<Renderer>().material = materials[3];
+    //    }
+    //    else if (hostPlayer.GetComponent<SpawningPlayer>().players[4] == null)
+    //    {
+    //        gameObject.name = "Player4";
+    //        hostPlayer.GetComponent<SpawningPlayer>().players[4] = playerPrefab;
+    //        currentPlayer.GetComponent<Renderer>().material = materials[4];
+    //    }
+    //    else
+    //    {
+    //        panelMoreThanFourPlayers.SetActive(true);
+    //    }
+    //}
+
+
+
     public PhotonView view;
-    public GameObject playerPrefab;
-    public GameObject currentPlayer;
-    public GameObject hostPlayer;
+    public GameObject panelFivePlayers;
+    public GameObject panelHostLeft;
     public GameObject[] players;
     public Material[] materials;
-    public GameObject panelHostLeave;
-    public GameObject panelMoreThanFourPlayers;
 
     public void Start()
     {
-        view = gameObject.GetComponent<PhotonView>();
-        panelHostLeave = GameObject.Find("HostLeave");
-        panelMoreThanFourPlayers = GameObject.Find("FivePlayers");
+        view = GetComponent<PhotonView>();
 
-        if(!view.IsMine)
+        if (!view.IsMine)
             return;
 
-        if (PhotonNetwork.CurrentRoom.PlayerCount != 1)
-            view.RPC("OtherPlayer", RpcTarget.All);
-
-        view.RPC("FirstPlayer", RpcTarget.All);
+        if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
+        {
+            view.RPC("HostClient", RpcTarget.All);
+            return;
+        }
+        view.RPC("NormalClients", RpcTarget.All);
     }
 
     public void Update()
     {
-        if(hostPlayer == null)
+        if (players[0] == null)
         {
-            panelHostLeave.SetActive(true);
+            view.RPC("HostLeft", RpcTarget.All);
         }
     }
 
-    public void FirstPlayer()
+    [PunRPC]
+    public void HostClient()
     {
-        currentPlayer = PhotonNetwork.Instantiate("PlayerCharacter Variant", transform.position, Quaternion.identity);
-        currentPlayer.GetComponent<Renderer>().material = materials[1];
-        players[PhotonNetwork.CurrentRoom.PlayerCount] = playerPrefab;
-        gameObject.name = "MainPlayer";
+        players[0] = PhotonNetwork.Instantiate("Player 1", transform.position, Quaternion.identity);
+        players[0].GetComponent<Renderer>().material = materials[0];
     }
 
-    public void OtherPlayer()
+    [PunRPC]
+    public void NormalClients()
     {
-        currentPlayer = PhotonNetwork.Instantiate("PlayerCharacter Variant", new Vector3 (0,1,0), Quaternion.identity);
-        hostPlayer = GameObject.Find("MainPlayer");
-
-        if (hostPlayer.GetComponent<SpawningPlayer>().players[2] == null)
+        if (players[1] != null)
         {
-            gameObject.name = "Player2";
-            hostPlayer.GetComponent<SpawningPlayer>().players[2] = playerPrefab;
-            currentPlayer.GetComponent<Renderer>().material = materials[2];
+            players[1] = PhotonNetwork.Instantiate("Player 1", transform.position, Quaternion.identity);
+            players[1].GetComponent<Renderer>().material = materials[1];
+            return;
         }
-        else if (hostPlayer.GetComponent<SpawningPlayer>().players[3] == null)
+        else if (players[2] != null)
         {
-            gameObject.name = "Player3";
-            hostPlayer.GetComponent<SpawningPlayer>().players[3] = playerPrefab;
-            currentPlayer.GetComponent<Renderer>().material = materials[3];
+            players[2] = PhotonNetwork.Instantiate("Player 1", transform.position, Quaternion.identity);
+            players[2].GetComponent<Renderer>().material = materials[2];
+            return;
         }
-        else if (hostPlayer.GetComponent<SpawningPlayer>().players[4] == null)
+        else if (players[3] != null)
         {
-            gameObject.name = "Player4";
-            hostPlayer.GetComponent<SpawningPlayer>().players[4] = playerPrefab;
-            currentPlayer.GetComponent<Renderer>().material = materials[4];
+            players[3] = PhotonNetwork.Instantiate("Player 1", transform.position, Quaternion.identity);
+            players[3].GetComponent<Renderer>().material = materials[3];
+            return;
         }
         else
         {
-            panelMoreThanFourPlayers.SetActive(true);
+            panelFivePlayers.SetActive(true);
         }
+    }
+
+    [PunRPC]
+    public void HostLeft()
+    {
+
     }
 }
