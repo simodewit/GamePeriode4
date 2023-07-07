6f90ecc8eb18ce5f5bbb863c32f8b723185af205
simodewit <s.f.buikema@gmail.com>
2023-07-07 15:27:42 +0100

    push

--- Game Met Sem/Assets/Scripts/WaveButton.cs
+++ Game Met Sem/Assets/Scripts/WaveButton.cs
@@ -1,13 +1,25 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WaveButton : MonoBehaviour
 {
+    public PhotonView view;
     public bool inWave;
     public WaveSpawnerScript waveSpawner;
 
+    public void Start()
+    {
+        view = GetComponent<PhotonView>();
+    }
+
     public void OnClickButton()
+    {
+        view.RPC("Abc", RpcTarget.All);
+    }
+
+    public void Abc()
     {
         inWave = true;
         waveSpawner.OnClickButton();
